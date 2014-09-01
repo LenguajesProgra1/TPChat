6b393df4259b2d8c4cb3b81ba90ca65e651400ce
jcarlos0305 <jcarlos0305@hotmail.com>
2014-08-31 18:20:29 -0600

    Server.c

--- server.c
+++ server.c
@@ -12,63 +12,101 @@
 
 #include <netinet/in.h>
 
-void dostuff(int); void error(const char *msg)
+#include <sys/unistd.h>
 
-{ perror(msg); exit(1); }
+#include <arpa/inet.h>
 
+FILE* f;
+
+void dostuff(int); 
+
+void error(const char *msg)
+{
+    perror(msg); 
+	exit(1);
+}
 
 int main(int argc, char *argv[])
-
 {
+	f	= fopen("Contactos","a+");
+	int sockfd, newsockfd, portno, pid; 
+	socklen_t clilen;
+	struct sockaddr_in serv_addr, cli_addr;
 
-int sockfd, newsockfd, portno, pid; socklen_t clilen;
+	if (argc < 2) 
+	{
+		fprintf(stderr,"ERROR, no hay ningún puerto\n");
+		exit(1);
+	}
 
-struct sockaddr_in serv_addr, cli_addr;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-if (argc < 2) {
+	if (sockfd < 0)
 
-fprintf(stderr,"ERROR, no hay ningún puerto\n"); exit(1); }
+		error("ERROR de apertura de socket"); 
+	
+	bzero((char *) &serv_addr, sizeof(serv_addr)); 
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_addr.s_addr = INADDR_ANY; 
+	serv_addr.sin_port = htons(portno);
 
-sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+		
+		error("ERROR en la conexión");
 
-if (sockfd < 0)
+	listen(sockfd,5); 
+	clilen = sizeof(cli_addr);
 
-error("ERROR de apertura de socket"); bzero((char *) &serv_addr, sizeof(serv_addr)); portno = atoi(argv[1]);
+	while (1) 
+	{
+		newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr, &clilen);
 
-serv_addr.sin_family = AF_INET; serv_addr.sin_addr.s_addr = INADDR_ANY; serv_addr.sin_port = htons(portno);
+		if (newsockfd < 0) 
 
-if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) error("ERROR en la conexión");
+			error("ERROR en aceptar"); 
+		
+		pid = fork();
 
-listen(sockfd,5); clilen = sizeof(cli_addr);
+		if (pid < 0) 
 
-while (1) {
+			error("ERROR en fork");
 
-newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr, &clilen);
-
-if (newsockfd < 0) error("ERROR en aceptar"); pid = fork();
-
-if (pid < 0) error("ERROR en fork");
-
-if (pid == 0)
-
-{ close(sockfd); dostuff(newsockfd); exit(0); } else close(newsockfd);
-
-} close(sockfd); return 0; }
-
-/*Gestiona todas las comunicaciones una vez que la conexión ha sido establecida*/
+		if (pid == 0)
+		{ 
+			close(sockfd); 
+			dostuff(newsockfd); 
+			exit(0);
+		} 
+		else
+			close(newsockfd);
+	} 
+	close(sockfd); 
+	return 0; 
+}
 
 void dostuff (int sock)
-
 {
- int n; char buffer[256];
+	int n,len_inet; 
+	char buffer[256], ip[14]; 
+	struct sockaddr_in addr_inet;    
 
-bzero(buffer,256); n = read(sock,buffer,255);
+	getpeername(sock, (struct sockaddr *)&addr_inet, &len_inet);
+	snprintf(ip,(sizeof(ip)), "%s", (inet_ntoa(addr_inet.sin_addr)));  	
 
-if (n < 0)
+	bzero(buffer,256);
+	n = read(sock,buffer,255);
+	if (n < 0)
+		error("ERROR al leer del socket");
+	
+	if(buffer[0]=='&')
+	{
+		
+		fprintf(f,"%s:%s\n",buffer,ip);
+	}
 
-error("ERROR al leer del socket");
-
-printf("Aquí está el mensaje: %s\n",buffer); n = write(sock,"Tengo el mensaje",18); if (n < 0)
-
-error("ERROR al escribir en el socket"); 
+	printf("Aquí está el mensaje: %s\n",buffer); 
+	n = write(sock,"Tengo el mensaje",18); 
+	if (n < 0)
+		error("ERROR al escribir en el socket"); 
 }
