f5ec1e87ae99c4c34e2d6f61c8314e66dabce5a6
marianaRamirez <marianaRamirez@users.noreply.github.com>
2014-09-02 08:29:57 -0600

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 
 GtkWidget *eNombre,*window,*help;// declaracion de variables
 int caracter;
-char *reg[3];
 
 void Validacion (GtkWidget *boton, GdkEventKey *event, GtkWidget *entry)//Validacion del entry del frame principal
 {
     int registro;
-   int largo = (int) g_utf8_strlen(gtk_entry_get_text(GTK_ENTRY(eNombre)), -1);
-   if ( largo != 0)
+    int largo = (int) g_utf8_strlen(gtk_entry_get_text(GTK_ENTRY(eNombre)), -1);
+    if ( largo != 0)
     {
         gchar *str = NULL;
         str = g_strconcat(gtk_entry_get_text(GTK_ENTRY(eNombre)), NULL);
@@ -32,66 +31,83 @@ void Validacion (GtkWidget *boton, GdkEventKey *event, GtkWidget *entry)//Valida
 }
 
 int main(int argc, char** argv) 
-{
-    char tmp[14];
-    char temporal[6];
-    FILE *f;
+{    
+    int pid,bandera=0;
+    pid = fork();
+    while(1)
+    {
+        if(pid<0)
+            g_print("ERROR en fork");
+        if(pid==0)
+        {
+            if(bandera==0)
+            {
+                bandera=1;
+                servidor(3,15557);
+            }
+        }
+        else
+        {
+            char tmp[14];
+            char temporal[6];
+            FILE *f;
 
-    reg[0] = "3";
-    
-    f = fopen("ipServidor.txt","r");
-    fgets(tmp,14,f);
-    reg[1] = tmp;    
-    fclose(f);    
-	
-    f = fopen("puerto.txt","r");
-    fgets(temporal,6,f);
-    reg[2] = temporal;    
-    fclose(f);
-    
-// Declaracion de los punteros de interfaz
-    GtkWidget *fixed;
-    GtkWidget *bSesion;
-    GtkWidget *lTitulo;
-    GtkWidget *lNombre;
-    gtk_init(&argc, &argv);
-    //Asignar los atributos a los punteros
-    
-    //Frame de inicio
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Chat");
-    gtk_window_set_default_size(GTK_WINDOW(window), 350, 120);
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    
-    //Layout
-    fixed = gtk_fixed_new();
-    gtk_container_add(GTK_CONTAINER(window), fixed);
-    
-    //Botones
-    bSesion = gtk_button_new_with_label("Iniciar Sesion");
-    gtk_fixed_put(GTK_FIXED(fixed), bSesion, 135, 80);
-    gtk_widget_set_size_request(bSesion, 80, 35);
-    
-    //Labels
-    lTitulo = gtk_label_new("Inicio de Sesion");
-    gtk_fixed_put(GTK_FIXED(fixed), lTitulo, 100, 0);
-    gtk_widget_set_size_request(lTitulo, 150, 35);
-    lNombre = gtk_label_new("Usuario :");
-    gtk_fixed_put(GTK_FIXED(fixed), lNombre, 10, 35);
-    gtk_widget_set_size_request(lNombre, 55, 35);
-    
-    //Entry
-    eNombre = gtk_entry_new();
-    gtk_fixed_put(GTK_FIXED(fixed), eNombre, 100, 40);
-    gtk_widget_set_size_request(eNombre, 80, 20);
-    
-    //Declaracion de señales
-    g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-    g_signal_connect(G_OBJECT(bSesion), "clicked", G_CALLBACK(Validacion), NULL);
-    gtk_widget_show_all(window);
-    
-    help = window;
-    
-    gtk_main();
-    return 0;
+            reg[0] = "3";
+
+            f = fopen("ipServidor.txt","r");
+            fgets(tmp,14,f);
+            reg[1] = tmp;    
+            fclose(f);    
+
+            f = fopen("puerto.txt","r");
+            fgets(temporal,6,f);
+            reg[2] = temporal;    
+            fclose(f);
+            // Declaracion de los punteros de interfaz
+            GtkWidget *fixed;
+            GtkWidget *bSesion;
+            GtkWidget *lTitulo;
+            GtkWidget *lNombre;
+            gtk_init(&argc, &argv);
+            //Asignar los atributos a los punteros
+
+            //Frame de inicio
+            window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+            gtk_window_set_title(GTK_WINDOW(window), "Chat");
+            gtk_window_set_default_size(GTK_WINDOW(window), 350, 120);
+            gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+            //Layout
+            fixed = gtk_fixed_new();
+            gtk_container_add(GTK_CONTAINER(window), fixed);
+
+            //Botones
+            bSesion = gtk_button_new_with_label("Iniciar Sesion");
+            gtk_fixed_put(GTK_FIXED(fixed), bSesion, 135, 80);
+            gtk_widget_set_size_request(bSesion, 80, 35);
+
+            //Labels
+            lTitulo = gtk_label_new("Inicio de Sesion");
+            gtk_fixed_put(GTK_FIXED(fixed), lTitulo, 100, 0);
+            gtk_widget_set_size_request(lTitulo, 150, 35);
+            lNombre = gtk_label_new("Usuario :");
+            gtk_fixed_put(GTK_FIXED(fixed), lNombre, 10, 35);
+            gtk_widget_set_size_request(lNombre, 55, 35);
+
+            //Entry
+            eNombre = gtk_entry_new();
+            gtk_fixed_put(GTK_FIXED(fixed), eNombre, 100, 40);
+            gtk_widget_set_size_request(eNombre, 80, 20);
+
+            //Declaracion de señales
+            g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+            g_signal_connect(G_OBJECT(bSesion), "clicked", G_CALLBACK(Validacion), NULL);
+            gtk_widget_show_all(window);
+
+            help = window;
+
+            gtk_main();
+            return 0;
+            }
+    }
 }
