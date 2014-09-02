787df845c65a2182289fc0df16b2f3ab6751ceea
marianaRamirez <marianaRamirez@users.noreply.github.com>
2014-09-02 08:31:03 -0600

    Update chat.c

--- chat.c
+++ chat.c
@@ -1,66 +1,72 @@
 #include "funciones.h"
 
 // creacion de variables globales
-GtkWidget* textArea;
-GtkWidget* boton;
-GtkWidget* textEntry;
-GtkTextBuffer *buffer;
-GtkTextIter start, end;
-GtkTextIter iter;
+char* inf;
+GtkWidget *eMsj;
 char *contacto;
 char* txt[1];
 
-char* enviado(GtkWidget *boton, GdkEventKey *event,GtkWidget *entry) // evento para enviar un mensaje al socket cliente
+char* enviar(GtkWidget *boton, GdkEventKey *event,GtkWidget *entry) // evento para enviar un mensaje al socket cliente
 {
-    // manejo de variables de la libreria gtk para mostrar el texto ingresado en el frame
-    const gchar* texto; 
-    
-    txt[0] = g_strconcat(contacto,gtk_entry_get_text(GTK_ENTRY(entry)),NULL);
-    texto = gtk_entry_get_text(GTK_ENTRY(entry));
-    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textArea));
-    gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
-    gtk_text_buffer_get_end_iter(buffer, &iter);
-    gtk_text_buffer_insert(buffer, &iter, texto , -1);
-    gtk_text_buffer_get_end_iter(buffer, &iter);
-    gtk_text_buffer_insert(buffer, &iter, " \n " , -1);
-    gtk_entry_set_text(GTK_ENTRY(textEntry),"");
-    return txt[0];
+    gchar *str = NULL;
+    int largo = (int) g_utf8_strlen(gtk_entry_get_text(GTK_ENTRY(eMsj)), -1);
+    if ( largo != 0)
+    {     
+        str = g_strconcat(contacto,":",gtk_entry_get_text(GTK_ENTRY(eMsj)),NULL);
+        //char tkn[(sizeof(str))];
+        //strcat(tkn,str);
+        reg[3] = str;
+        gtk_entry_set_text(GTK_ENTRY(eMsj),"");
+        Cliente(3,reg);
+    }
 }
 
-GtkWidget* chatFrame()// crea el frame para el chat
-{
-// manejo de variables de la libreria gtk para crear el chatFrame
-    
-textEntry = gtk_entry_new();
-textArea = gtk_text_view_new();
-GtkWidget* scrollbar= gtk_vscrollbar_new(gtk_text_view_get_vadjustment(GTK_TEXT_VIEW(textArea)));
-GtkWidget* console = gtk_table_new(3, 2, FALSE);
-gtk_text_view_set_editable(G_OBJECT(textArea),0);
-boton = gtk_button_new_with_label("Enviar");
-gtk_table_attach_defaults(GTK_TABLE(console), textArea, 0, 4, 0, 1);
-gtk_table_attach_defaults(GTK_TABLE(console), scrollbar, 0, 4, 0, 1);
-gtk_table_attach_defaults(GTK_TABLE(console), textEntry, 0, 3, 1, 2);
-gtk_table_attach_defaults(GTK_TABLE(console), boton, 3,4,1,2);
-gtk_widget_set_size_request(textArea, 320, 240);
-gtk_widget_grab_focus(textEntry);
-return console;
-}
 
-int chat(int argc,char* argv[],char *nom)
+int chat(int argc,char *argv[],char *nom)//,char* info[])
 {
-    //creacion del frame principal sobre el que se va a encontrar la ventana para enviar mensajes
+    //creacion de la ventana para enviar mensajes
     contacto = nom;
+    //inf = info;
     GtkWidget *window;
     gtk_init(&argc, &argv);
+    GtkWidget *fixed;
+    GtkWidget *bEnviar;
+    GtkWidget *lTitulo;
+
+  
+    //Asignar los atributos a los punteros
+    
+    //Frame de inicio
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Mensajeria");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_default_size(GTK_WINDOW(window), 350, 120);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_container_add(GTK_CONTAINER(window), chatFrame());
+    
+    //Layout
+    fixed = gtk_fixed_new();
+    gtk_container_add(GTK_CONTAINER(window), fixed);
+    
+    //Botones
+    bEnviar = gtk_button_new_with_label("Enviar");
+    gtk_fixed_put(GTK_FIXED(fixed), bEnviar, 135, 80);
+    gtk_widget_set_size_request(bEnviar, 80, 35);
+    
+    //Labels
+    lTitulo = gtk_label_new("Escriba su mensaje:");
+    gtk_fixed_put(GTK_FIXED(fixed), lTitulo, 100, 0);
+    gtk_widget_set_size_request(lTitulo, 150, 35);
+
+    
+    //Entry
+    eMsj = gtk_entry_new();
+    gtk_fixed_put(GTK_FIXED(fixed), eMsj, 100, 40);
+    gtk_widget_set_size_request(eMsj, 100, 30);
+    
+    //Declaracion de señales
+    g_signal_connect(G_OBJECT (window), "destroy",G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(G_OBJECT(bEnviar), "clicked", G_CALLBACK(enviar), NULL);
     gtk_widget_show_all(window);
-    gtk_window_set_resizable(GTK_WINDOW(window),FALSE);
-    g_signal_connect (G_OBJECT (window), "destroy",G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect (G_OBJECT(boton), "clicked",G_CALLBACK(enviado), G_OBJECT(textEntry));
+        
     gtk_main();
     return 0;
 }
